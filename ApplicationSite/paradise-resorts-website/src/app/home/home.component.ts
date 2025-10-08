53ea8c0408c6585c5ca51eb96b14a8320968e73d
mariosegaly <mariosegaly4@gmail.com>
2025-10-07 19:17:19 -0600

    Mejoras y se agregó la hoja de contacto y link provisional a la app.

--- ApplicationSite/paradise-resorts-website/src/app/home/home.component.ts
+++ ApplicationSite/paradise-resorts-website/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +11,24 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
 
+   //constructor(private router: Router) {}
+
   images = [
   'assets/hotel_imagen1.jpg',
+  'assets/hotel_imagen1.jpg',
   'assets/hotel_imagen2.jpg',
-  'assets/hotel_imagen3.jpg'
+  'assets/hotelcolonial1.jpg',
+  'assets/hotelcolonial2.jpg',
+  'assets/hotelcolonial3.jpg',
+  'assets/hotelmiramar1.jpg',
+  'assets/hotelmiramar2.jpg',
+  'assets/hotelmiramar3.jpg',
+  'assets/hotelpacifico1.jpg',
+  'assets/hotelpacifico2.jpg',
+  'assets/hotelpacifico3.jpg',
+  'assets/hotelvolcanes1.jpg',
+  'assets/hotelvolcanes2.jpg',
+  'assets/hotelvolcanes3.jpg',
 ];
 
 
@@ -25,6 +40,12 @@ export class HomeComponent {
 
   prevSlide() {
     this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  openLinkOfPlayStoreWebSite() {
+    //this.router.navigateByUrl("")
+    const urlExterna = 'https://play.google.com/store/apps/details?id=com.palaceresorts.app&pli=1';
+    window.open(urlExterna, '_blank');
   }
 
 }
